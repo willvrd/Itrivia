f5281e89162e0c3f546588ea7095ab4eac3481da
William Verde <williamverde10@gmail.com>
2019-10-31 11:06:40 -0400

    Add: point to Trivia

--- Events/Handlers/CheckQuestionsAnswers.php
+++ Events/Handlers/CheckQuestionsAnswers.php
@@ -7,10 +7,14 @@ class CheckQuestionsAnswers
 {
    
     private $userQARepository;
+    private $setting;
+    private $pointRepository;
 
     public function __construct()
     {
         $this->userQARepository = app('Modules\Itrivia\Repositories\UserQuestionAnswerRepository');
+        $this->setting = app('Modules\Setting\Contracts\Setting');
+        $this->pointRepository =  app('Modules\Iredeems\Repositories\PointRepository');
     }
 
     public function handle($event)
@@ -18,65 +22,95 @@ class CheckQuestionsAnswers
         $userTrivia = $event->userTrivia;
         $data = $event->data;
 
-        $qOk = 0;
+        // Settings Iredeems
+        $checkTrivia =  $this->setting->get('iredeems::points-per-finished-trivia-checkbox');
 
-        // Get questions for the Trivia
-        $questions = $userTrivia->trivia->questions;
-        foreach ($questions as $key => $question) {
-            //echo "Pregunta: ".$question->title."<br>";
-            $answers = $question->answers;
-            // Get Correct Answer
-            $answerId = null;
-            foreach($answers as $answer){
-                if($answer->correct){
-                    //echo "*Respuesta correcta: ".$answer->id."<br>";
-                    $answerId = $answer->id;
-                    break;
-                }    
-            }
+        // Points Actives
+        if($checkTrivia){
 
-            // Get answer from User
-            $attributes = array(
-                "user_id" => $userTrivia->user_id,
-                "question_id" => $question->id
-            );
-            $result = $this->userQARepository->findByAttributes($attributes);
-            //echo "*Respuesta Usuario: ".$result->answer_id."<br>";
-            // Check if the answer was correct
-            if($result->answer_id==$answerId){
-                //echo "***Respuesta Acertada ***<br>";
-                $qOk++;
-            }
-        }// End Foreach
+            $qOk = 0;
 
-        // Results
-        $totalQuestions = count($questions);
-        $porcentComplete = $qOk * 100 /  $totalQuestions;
-        //echo "<br> Total de Preguntas: {$totalQuestions} <br>";
-        //echo "Acertadas:{$qOk} <br>";
-        //echo "Porcentaje Completado: {$porcentComplete} % <br><br>";
+            // Get questions for the Trivia
+            $questions = $userTrivia->trivia->questions;
 
-        // Range Points
-        $sumPoints = 0;
-        $rangePoints = $userTrivia->trivia->rangePoints;
-
-        $sorted = $rangePoints->sortByDesc('value');
-
-        foreach ($sorted as $key => $range) {
-            //echo "Rango de puntos para:".$range->value." % Completado - Puntos: ".$range->points."<br>";
-            if($range->value==$porcentComplete){
-                $sumPoints = $range->points;
-                break;
-            }else{
-                // If porcentaje complete isn't exactly but is major than other
-                if($porcentComplete>=$range->value){
-                    $sumPoints = $range->points;
-                    break; 
+            foreach ($questions as $key => $question) {
+                //echo "Pregunta: ".$question->title."<br>";
+                $answers = $question->answers;
+                // Get Correct Answer
+                $answerId = null;
+                foreach($answers as $answer){
+                    if($answer->correct){
+                        //echo "*Respuesta correcta: ".$answer->id."<br>";
+                        $answerId = $answer->id;
+                        break;
+                    }    
                 }
+
+                // Get answer from User
+                $attributes = array(
+                    "user_id" => $userTrivia->user_id,
+                    "question_id" => $question->id
+                );
+                $result = $this->userQARepository->findByAttributes($attributes);
+                //echo "*Respuesta Usuario: ".$result->answer_id."<br>";
+                // Check if the answer was correct
+                if($result->answer_id==$answerId){
+                    //echo "***Respuesta Acertada ***<br>";
+                    $qOk++;
+                }
+            }// End Foreach
+
+            // Results
+            $totalQuestions = count($questions);
+            $porcentComplete = $qOk * 100 /  $totalQuestions;
+            //echo "<br> Total de Preguntas: {$totalQuestions} <br>";
+            //echo "Acertadas:{$qOk} <br>";
+            //echo "Porcentaje Completado: {$porcentComplete} % <br><br>";
+
+            // Range Points
+            $sumPoints = 0;
+            $rangePoints = $userTrivia->trivia->rangePoints;
+
+            // Trivia has Range Points
+            if(count($rangePoints)>0){
+                $sorted = $rangePoints->sortByDesc('value');
+
+                foreach ($sorted as $key => $range) {
+                    //echo "Rango de puntos para:".$range->value." % Completado - Puntos: ".$range->points."<br>";
+                    if($range->value==$porcentComplete){
+                        $sumPoints = $range->points;
+                        break;
+                    }else{
+                        // If porcentaje complete isn't exactly but is major than other
+                        if($porcentComplete>=$range->value){
+                            $sumPoints = $range->points;
+                            break; 
+                        }
+                    }  
+                }
+                
+            }else{
+
+                //Trivia Doesn't have Range points, Take setting
+                $pointsTrivia = $this->setting->get('iredeems::points-per-finished-trivia');
+                if($porcentComplete==100)
+                    $sumPoints = (int)$pointsTrivia;
+
             }
-               
+            //echo "Puntos para el Usuario: {$sumPoints} <br>"; 
+            
+            // Save Points
+            $class=\Modules\Itrivia\Entities\Trivia::class;
+            $data = array(
+                "user_id"=> (int)$userTrivia->user_id,
+                "pointable_id" => (int)$userTrivia->trivia_id,
+                "pointable_type" => $class,
+                "points" => (int)$sumPoints,
+                "description" => trans("iredeems::common.settingsMsg.points-per-trivia")
+            );
+            $this->pointRepository->create($data);
+
         }
-        //echo "Puntos para el Usuario: {$sumPoints} <br>";
 
     }// If handle
 
